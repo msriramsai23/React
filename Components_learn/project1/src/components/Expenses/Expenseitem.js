fa49133d80526de5de769d20698c42d76f39c18c
msriramsai23 <m.sriramsai23@gmail.com>
2023-03-22 16:25:04 +0530

    1

--- Components_learn/project1/src/components/Expenses/Expenseitem.js
+++ Components_learn/project1/src/components/Expenses/Expenseitem.js
@@ -1,10 +1,15 @@
+import {useState} from "react";
 import "./Expenseitem.css";
-
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 //using props
 function ExpenseItem(props) {
+  const [title,setTitle]=useState(props.title); 
+  
+  const clickhandler = () =>{
+    setTitle("clicked !!!");
+    }
   //Return  statement  must be wrapped in div
   return (
     //className instead of class
@@ -12,9 +17,10 @@ function ExpenseItem(props) {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickhandler}>click!</button>
     </Card>
   );
 }
