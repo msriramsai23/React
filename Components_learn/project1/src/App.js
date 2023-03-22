f3cb48f0f0403537dc94222365fabbfd7386c047
msriramsai23 <m.sriramsai23@gmail.com>
2023-03-22 16:24:11 +0530

    updated

--- Components_learn/project1/src/App.js
+++ Components_learn/project1/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -26,7 +27,8 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses items={expenses} />
+      <NewExpense/>
+      <Expenses items={expenses} /> 
     </div>
   );
 }
