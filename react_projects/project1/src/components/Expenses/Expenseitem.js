6aa308b9d7a7cc6dcefee0e5c44d6f99b9dac00e
msriramsai23 <m.sriramsai23@gmail.com>
2023-04-17 11:32:03 +0530

    1

--- react_projects/project1/src/components/Expenses/Expenseitem.js
+++ react_projects/project1/src/components/Expenses/Expenseitem.js
@@ -1,27 +1,21 @@
-import {useState} from "react";
-import "./Expenseitem.css";
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
 
-//using props
-function ExpenseItem(props) {
-  const [title,setTitle]=useState(props.title); 
-  
-  const clickhandler = () =>{
-    setTitle("clicked !!!");
-    }
-  //Return  statement  must be wrapped in div
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './Expenseitem.css';
+
+const ExpenseItem = (props) => {
   return (
-    //className instead of class
-
-    <Card className="expense-item">
+    <li>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <div className="expense-item__price">${props.amount}</div>
-      <button onClick={clickhandler}>click!</button>
     </Card>
+    </li>
   );
 }
 
